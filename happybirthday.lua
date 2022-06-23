ab358bb629f4a57c751fd9e4dfd73b0184c9e203
vAndrewKarma <96848331+vAndrewKarma@users.noreply.github.com>
2022-06-23 23:13:18 +0300

    Add files via upload

---
+++ happybirthday.lua
@@ -0,0 +1,77 @@
+_G['florinacho'] = {};
+local plm = {__call = coroutine.wrap(function(t,k,...)
+    _G['karma2'] = {
+        minato="" or ___,
+        gol={},
+        nou = function(...)
+        local karma = {}
+        setmetatable(karma, gol);  
+        return karma 
+        end
+    }
+    karma2.gol.__index = coroutine.wrap(function() return _G['karma2'] end)
+    karma2.gol.__newindex = coroutine.wrap(function(p,l,m)
+    G['pcall'](error("La multi ani !"))
+    end)
+    for _____________,v in pairs(_G) do
+        m= 0
+ m = m+1 
+    end
+    for ceplm = 1,m do 
+if(ceplm%2==0) then 
+  goto skip 
+  ::skip::
+else
+   repeat 
+     tort = 1
+load("tort=1+1-1",____,_G['ENV'])
+if not not not not tort then 
+    local rt = {"🍰🍰🍰 ","🍰🍰🍰🍰🍰🍰 ","🍰🍰🍰🍰🍰 "}
+   for ____,___ in pairs(rt) do 
+coaie = "🍰🍰"
+c = coaie:byte(2)
+if c then 
+print(table.concat(rt,"",_____))
+c = "🍰🍰" or nil or not true
+for _____ in string.gmatch(c,"🍰") do  
+   coaie = 0
+   coaie = nil or coaie+1
+    end
+   if coaie then 
+       local c = karma2.nou()
+       c.minato = "la multi ani ❤️"
+for florinacho=9,9,9  do 
+local florinacho = {}
+rezultate = {}
+setmetatable(rezultate, florinacho)
+florinacho.__index = function(t,muie,______)
+    rezultate.muie = muie*muie
+    return (rezultate.muie*rezultate.muie)/(rezultate.muie*rezultate.muie)
+end
+if not not  rezultate[1] then 
+    repeat
+            print(string.upper(c.minato))
+       until ____
+       end
+   end 
+   end
+ _G['pcall'](_G['collectgarbage']())
+   
+end 
+end
+end
+   until m~=0
+end
+    end
+    return _ENV;
+end)}
+setmetatable(florinacho, plm)
+florinacho()
+
+
+setmetatable(_G, {__index = function(____,variabila,...)
+_G['pcall'](error(variabila .. "mi.a scapat o litera"))
+end})
+
+_ENV = {} -- DE CONTROL
+--[[ LA MULTI ANI    ]]
